ae7f05ab89f5caa63e0e5537d7671a6ec141f520
CVhnu <164332852+CVhnu@users.noreply.github.com>
2024-12-12 11:45:06 +0800

    Add files via upload

---
+++ code/data/base_dataset.py
@@ -0,0 +1,90 @@
+import torch.utils.data as data
+from PIL import Image
+import torchvision.transforms as transforms
+from torchvision.transforms import InterpolationMode
+import numpy as np
+import random
+
+
+class BaseDataset(data.Dataset):
+    def __init__(self):
+        super(BaseDataset, self).__init__()
+
+    def name(self):
+        return 'BaseDataset'
+
+    def initialize(self, opt):
+        pass
+
+
+def get_params(opt, size):
+    w, h = size
+    new_h = h
+    new_w = w
+    if opt.resize_or_crop == 'resize_and_crop':
+        new_h = new_w = opt.loadSize            
+    elif opt.resize_or_crop == 'scale_width_and_crop':
+        new_w = opt.loadSize
+        new_h = opt.loadSize * h // w
+
+    x = random.randint(0, np.maximum(0, new_w - opt.fineSize))
+    y = random.randint(0, np.maximum(0, new_h - opt.fineSize))
+    
+    flip = random.random() > 0.5
+    return {'crop_pos': (x, y), 'flip': flip}
+
+
+def get_transform(osize, opt, method=InterpolationMode.BICUBIC, normalize=True):
+    transform_list = []
+    if 'resize' in opt.resize_or_crop:
+        # osize = [opt.loadSize, opt.loadSize]
+        transform_list.append(transforms.Resize(osize, method))
+    elif 'scale_width' in opt.resize_or_crop:
+        transform_list.append(transforms.Lambda(lambda img: __scale_width(img, opt.loadSize, method)))
+
+    if opt.resize_or_crop == 'none':
+        pass
+
+    transform_list += [transforms.ToTensor()]
+
+    if normalize:
+        transform_list += [transforms.Normalize((0.5, 0.5, 0.5),
+                                                (0.5, 0.5, 0.5))]
+    return transforms.Compose(transform_list)
+
+
+def normalize():    
+    return transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+
+
+def __make_power_2(img, base, method=InterpolationMode.BICUBIC):
+    ow, oh = img.size        
+    h = int(round(oh / base) * base)
+    w = int(round(ow / base) * base)
+    if (h == oh) and (w == ow):
+        return img
+    return img.resize((w, h), method)
+
+
+def __scale_width(img, target_width, method=InterpolationMode.BICUBIC):
+    ow, oh = img.size
+    if (ow == target_width):
+        return img    
+    w = target_width
+    h = int(target_width * oh / ow)    
+    return img.resize((w, h), method)
+
+
+def __crop(img, pos, size):
+    ow, oh = img.size
+    x1, y1 = pos
+    tw = th = size
+    if (ow > tw or oh > th):        
+        return img.crop((x1, y1, x1 + tw, y1 + th))
+    return img
+
+
+def __flip(img, flip):
+    if flip:
+        return img.transpose(Image.FLIP_LEFT_RIGHT)
+    return img
---
+++ code/examples/test.py
@@ -0,0 +1,93 @@
+from __future__ import print_function, absolute_import
+import argparse
+import os
+import random
+import numpy as np
+import torch
+import yaml
+import torchvision
+from torch import nn
+from torch.backends import cudnn
+from examples.model import DehazeNet
+from gcl.utils.gan_utils import get_config, denormalize1
+from data.data_loader import CreateDataLoader
+
+def main():
+    args = parser.parse_args()
+
+    if args.seed is not None:
+        random.seed(args.seed)
+        np.random.seed(args.seed)
+        torch.manual_seed(args.seed)
+        cudnn.deterministic = True
+    main_worker(args)
+
+
+def main_worker(args):
+
+    cudnn.benchmark = True
+
+    config = get_config(args.config)
+
+    print("==========\nArgs:{}\n==========".format(args))
+
+    # data load
+    data_loader_train = CreateDataLoader(args)
+    dataset_train = data_loader_train.load_data()
+    dataset_size = len(data_loader_train)
+    print('#testing images = %d' % dataset_size)
+
+    # Trainer
+    trainer = DehazeNet(config).cuda()
+
+    model_path = './pretrained/SSID.pt'
+    state_dict = torch.load(model_path)
+    trainer.gen.load_state_dict(state_dict['gen'])
+
+
+    for m in trainer.modules():
+        for child in m.children():
+            if type(child) == nn.BatchNorm2d:
+                child.track_running_stats = False
+
+    if torch.cuda.device_count() > 1:
+        print("Let's use", torch.cuda.device_count(), "GPUs!")
+
+        trainer = nn.DataParallel(trainer, device_ids=range(torch.cuda.device_count()))
+
+    with torch.no_grad():
+        for it, data in enumerate(dataset_train):
+            pseudo_hazy = data['hazy'].cuda()
+            dehazed = trainer.module.sample(pseudo_hazy)
+            syn_hazy = denormalize1(dehazed)
+            torchvision.utils.save_image(syn_hazy, os.path.join(args.output, '{}.png'.format(it)))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="")
+    # data
+    parser.add_argument('-b', '--batchSize', type=int, default=1)
+    parser.add_argument('--serial_batches', action='store_true',
+                        help='if true, takes images in order to make batches, otherwise takes them randomly')
+    parser.add_argument('--nThreads', default=16, type=int, help='# threads for loading data')
+    parser.add_argument('--dataroot', type=str, default='./input/')
+    parser.add_argument('--output', type=str,
+                        default='./output/')
+    parser.add_argument('--max_dataset_size', type=int, default=float("inf"),
+                        help='Maximum number of samples allowed per dataset. If the dataset directory contains more than max_dataset_size, only a subset is loaded.')
+    parser.add_argument('--resize_or_crop', type=str, default='resize',
+                        help='scaling and cropping of images at load time [resize_and_crop|crop|scale_width|scale_width_and_crop]')
+    parser.add_argument('--loadSize', type=int, default=512, help='scale images to this size')
+
+    parser.add_argument('--isTrain', default=False, help='testing')
+
+    # gan config
+    parser.add_argument('--config', type=str, default='../configs/latest.yaml', help='Path to the config file.')
+    parser.add_argument('--output_path', type=str, default='.', help="outputs path")
+    parser.add_argument('--name', type=str, default='latest', help="outputs path")
+
+    parser.add_argument('--epochs', type=int, default=200)
+    parser.add_argument('--seed', type=int, default=1)
+    parser.add_argument("--resume", default=True)
+
+    main()
